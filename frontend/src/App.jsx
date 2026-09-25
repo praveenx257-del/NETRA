b7dbe619739f8bc996f10864c030931819eda82c
praveenx257-del <praveenx257@gmail.com>
2026-09-26 01:58:40 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ShieldCheck, AlertCircle, Camera, CheckSquare, Layers, Send } from 'lucide-react';
 
-const API_BASE = 'http://127.0.0.1:8000';
+const API_BASE = 'https://netra-backend-kmke.onrender.com';
 
 export default function App() {
   const [form, setForm] = useState({
